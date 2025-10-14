a193e4c38ee6cc12a8cec218842f50d6bd938dd7
Giulia <giuliagiacomodonato1@gmail.com>
2025-10-14 20:18:43 -0300

    :()

--- app/api/canchas/route.ts
+++ app/api/canchas/route.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
-import { TipoCancha } from '@prisma/client'
 
 interface CanchaData {
   nombre: string
