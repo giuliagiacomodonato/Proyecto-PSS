dcc51bedd14fde2f96a297199e1212c483eec76f
Giulia <giuliagiacomodonato1@gmail.com>
2025-10-14 19:44:46 -0300

    intento deploy

---
+++ app/admin/bajaSocio/page.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '@/app/components/Sidebar'
+
+export default function BajaSocioPage() {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      
+      <main className="flex-1 p-8">
+        {/* TODO: Implementar funcionalidad de baja de socio */}
+      </main>
+    </div>
+  )
+}
